1248419ec5f966bee8780df0c27f1646279fa2a4
hohno-46466 <hohno.46466@gmail.com>
2023-12-05 14:13:32 +0900

    added/updated by @hohno_at_kuimc (Tue Dec  5 14:13:32 JST 2023)

---
+++ misc/Lesson_0and1/Lesson_0and1.ino
@@ -0,0 +1,20 @@
+//
+// test00.ino
+//
+
+// Last update: Thu Oct  5 16:50:55 JST 2023
+
+#undef LED_BUILTIN
+#define LED_BUILTIN 4
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+ 
